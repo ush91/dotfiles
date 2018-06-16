c9118836d77373056bcbb2cdf973bef1e8c25e44
ush91 <ush91@ignore.git>
2018-06-16 23:52:26 +0900

    add company-cmake to cmake-mode-hook

--- init.el
+++ init.el
@@ -231,6 +231,10 @@
 (autoload 'cmake-mode "cmake-mode" nil t)
 (add-to-list 'auto-mode-alist '("CMakeLists\\.txt\\'" . cmake-mode))
 (add-to-list 'auto-mode-alist '("\\.cmake\\'" . cmake-mode))
+(when (package-installed-p 'company)
+  (add-hook 'cmake-mode-hook
+            '(lambda ()
+               (add-to-list 'company-backends 'company-cmake))))
 
 
 (setq compilation-scroll-output t
